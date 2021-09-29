de46cb3f21cacaeaa2f160d896ca51ad0cba0c40
Даниил Пустовалов <pustden524@gmail.com>
2021-09-29 12:29:30 +0500

    Пустовалов 2пк2

---
+++ pz_6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace pz_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое положительное число: "); //запрашиваем число
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = 0;
+            int i = 1;
+
+
+            while (i <= a)
+            {
+                b = b + i;
+                i = i + 1;
+            }           
+            Console.WriteLine(b);
+        }
+    }
+}
