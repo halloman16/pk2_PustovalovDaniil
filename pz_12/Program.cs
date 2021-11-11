22166aa9a7558a30350d0527a9f9fad87f41fe27
Даниил Пустовалов <pustden524@gmail.com>
2021-11-12 00:44:20 +0500

    pz_12

---
+++ pz_12/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace pz_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = "у Вани было 11 яблок, а у Оли 12";
+            var words = data.Split();
+            int i;
+            int n = 0;
+            List<int> integers = new List<int>();
+            foreach (var s in words)
+            {
+                if (int.TryParse(s, out i)) { integers.Add(i); }
+                n += i;
+            }
+            Console.WriteLine(n);
+        }
+    }
+}
