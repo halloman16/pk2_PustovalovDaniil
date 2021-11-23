09a722bafca56d9340861bb239d9239cbd3ea765
Даниил Пустовалов <pustden524@gmail.com>
2021-11-23 19:51:08 +0500

    pz_14

---
+++ pz_14/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace pz_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\SomeDir";
+            string subpath = @"new";
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+            dirInfo.CreateSubdirectory(subpath);
+
+            
+            var myFile = File.Create(@"C:\SomeDir\new\f2.txt");
+
+            myFile.Close();
+
+            File.AppendAllText(@"C:\SomeDir\new\f2.txt", File.ReadAllText(@"C:\SomeDir\f1.txt"));
+
+        }
+    }
+}
