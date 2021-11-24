a67952b8df0962f21cbc5d0173f363826396b823
Даниил Пустовалов <pustden524@gmail.com>
2021-11-24 17:45:15 +0500

    pz_15

---
+++ pz_15/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace pz_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine(); // спрашиваем слово
+                       
+            string[] mas = File.ReadAllLines(@"C:\SomeDir\f1.txt");           
+            foreach (string a in mas) // ищем слово в строчках
+            {
+                if (a.Contains(s))
+                {
+                    Console.WriteLine(a); // выводим слово                   
+                }
+            }                       
+        }
+    }
+}
