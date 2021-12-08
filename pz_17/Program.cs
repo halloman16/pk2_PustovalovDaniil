b143c6ab3c5f374024a713a8afe354a2c9634e14
Даниил Пустовалов <pustden524@gmail.com>
2021-12-08 19:56:09 +0500

    pz_17

---
+++ pz_17/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace pz_17
+{
+    class Program
+    {
+        static int[] Param(int[] nums)
+        {
+            int n = 0;          
+            foreach (int i in nums)
+            {
+                if (i < 0)
+                {                   
+                    n++;                    
+                }               
+            }
+            Console.WriteLine("Количество дней, когда температура была ниже 0*C: "+n);
+            return nums; 
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количесво дней: ");
+            int elements = int.Parse(Console.ReadLine());
+            Console.WriteLine("Вводите значения температуры в эти дни: ");
+            int[] myArr = new int[elements];            
+            for (int i = 0; i < myArr.Length; i++) 
+            {
+                myArr[i] = int.Parse(Console.ReadLine()); 
+            }
+            Param(myArr); 
+        }
+    }
+}
