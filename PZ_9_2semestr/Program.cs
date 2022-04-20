76f63cdb210ca43ec62c5302b3674b9666f451a5
Даниил Пустовалов <pustden524@gmail.com>
2022-04-20 16:23:40 +0500

    new pz

---
+++ PZ_9_2semestr/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ_9_2semestr
+{
+    public delegate void Delegate();
+
+    class Counter
+    {
+        public event Delegate onCount;
+        public void Count()
+        {
+            for (int i = 0; i < 35; i++)
+            {
+                Console.WriteLine(i);
+                if (i == 30)
+                {
+                    onCount();
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+
+    public static class Program
+    {
+
+        public static void Message()
+        {
+            Console.WriteLine("Уведомление! Ограничение по визитам - 30!");
+        }
+
+        static void Main(string[] args)
+        {
+            Counter counter = new Counter();
+
+            counter.onCount += Message;
+
+            counter.Count();
+
+        }
+        
+    }
+}
